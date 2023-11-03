0548be4c51972cb6409092918e3b15966fbb55dc
Kayla Weaver <kaylabeth1211@gmail.com>
2023-11-02 17:56:16 -0700

    all views

---
+++ LaMasDulce/Controllers/TreatsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using LaMasDulce.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LaMasDulce.Controllers
+{
+  public class TreatsController : Controllers
+  {
+    private readonly LaMasDulceContext _db;
+    
+  }
+}
